d3d1fc473d5ccf20f903e08f50fd9506f4454f0d
MaxWxaM <116869653+MaxWxaM@users.noreply.github.com>
2022-11-06 22:11:32 +0800

    Update 21.merge-two-sorted-lists.go
    Use recursive

--- 21.merge-two-sorted-lists.go
+++ 21.merge-two-sorted-lists.go
@@ -19,34 +19,14 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list2 == nil {
 		return list1
 	}
-	var head, current *ListNode
+
 	if list1.Val < list2.Val {
-		head = &ListNode{Val: list1.Val}
-		list1 = list1.Next
-	} else {
-		head = &ListNode{Val: list2.Val}
-		list2 = list2.Next
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
 	}
-	current = head
-	for {
-		if list1 == nil {
-			current.Next = list2
-			break
-		} else if list2 == nil {
-			current.Next = list1
-			break
-		} else {
-			if list1.Val < list2.Val {
-				current.Next = list1
-				list1 = list1.Next
-			} else {
-				current.Next = list2
-				list2 = list2.Next
-			}
-			current = current.Next
-		}
-	}
-	return head
+
+	list2.Next = mergeTwoLists(list2.Next, list1)
+	return list2
 }
 
 // @lc code=end
