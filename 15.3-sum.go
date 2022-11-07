eef6813a9de56f68d7d1fcfbdbe3a105b4a24840
MaxWxaM <ming81514m@gmail.com>
2022-11-07 14:38:37 +0800

    Create 15.3-sum.go
    Init version

---
+++ 15.3-sum.go
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=15 lang=golang
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+func threeSum(nums []int) [][]int {
+	var result [][]int
+	sort.Ints(nums)
+	current, left, right := 0, 1, len(nums)-1
+	var currentVal int
+	for nums[current] <= 0 {
+		currentVal = nums[current]
+		for left < right {
+			if nums[left]+nums[right]+currentVal > 0 {
+				for nums[right] == nums[right-1] {
+					right--
+					if left >= right {
+						break
+					}
+				}
+				right--
+				continue
+			}
+			if nums[left]+nums[right]+currentVal < 0 {
+				for nums[left] == nums[left+1] {
+					left++
+					if left >= right {
+						break
+					}
+				}
+				left++
+				continue
+			}
+			if nums[left]+nums[right]+currentVal == 0 {
+				result = append(result, []int{nums[left], nums[right], currentVal})
+				for nums[left] == nums[left+1] {
+					left++
+					if left >= right {
+						break
+					}
+				}
+				left++
+				continue
+			}
+		}
+		for nums[current] == nums[current+1] {
+			current++
+			if current == len(nums)-2 {
+				return result
+			}
+		}
+		current++
+		if current == len(nums)-2 {
+			return result
+		}
+		left = current + 1
+		right = len(nums) - 1
+	}
+	return result
+}
+
+// @lc code=end
+
