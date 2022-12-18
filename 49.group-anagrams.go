fe7ebb6c1f5525234738a3ad17a1c218a6ac42ad
MaxWxaM <116869653+MaxWxaM@users.noreply.github.com>
2022-12-18 11:14:51 +0800

    Update 49.group-anagrams.go
    Second Version:
    1. 開個map去做記錄,
    	=>key 是一個array, 裡面包涵26個英文字母出現字數（index = 英文字母, value = count)
    	=>value, string的slice
    2. 答案就是map的所有value, append 到slice就ok

--- 49.group-anagrams.go
+++ 49.group-anagrams.go
@@ -6,31 +6,28 @@
 
 // @lc code=start
 /*
-Init version,
-1. 開個map去做記錄, key 是排序過後的字串, value則是slice的位置
-2. 用for迴圈遍歷所有str, 先排序過後檢查map中有沒有存在, 已存在就拿map的value去array中append
+Second version,
+1. 開個map去做記錄,
+	=>key 是一個array, 裡面包涵26個英文字母出現字數（index = 英文字母, value = count)
+	=>value, string的slice
+2. 答案就是map的所有value, append 到slice就ok
 
 */
 
 func groupAnagrams(strs []string) [][]string {
-	m := make(map[string]int) // key is
-	var result [][]string
-	for _, v := range strs {
-		sorted := SortString(v)
-		if loca, ok := m[sorted]; ok {
-			result[loca] = append(result[loca], v)
-			continue
+	m := make(map[[26]int][]string)
+	for _, str := range strs {
+		curr := [26]int{}
+		for i := 0; i < len(str); i++ {
+			curr[str[i]-'a']++
 		}
-		m[sorted] = len(result)
-		result = append(result, []string{v})
+		m[curr] = append(m[curr], str)
+	}
+	var result [][]string
+	for _, v := range m {
+		result = append(result, v)
 	}
 	return result
-}
-
-func SortString(w string) string {
-	s := strings.Split(w, "")
-	sort.Strings(s)
-	return strings.Join(s, "")
 }
 
 // @lc code=end
